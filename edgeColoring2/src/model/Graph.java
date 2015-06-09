7dd9266e8421624ee02f7fcff7e686692656d002
Xenir <xenirek@gmail.com>
2015-06-09 15:12:27 +0200

    todo GRAPH

---
+++ edgeColoring2/src/model/Graph.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.LinkedList;
+
+public class Graph {
+	// TODO:
+	// to wszystko MUSI byæ jako graf, bo nie wiem nawet jak mam siê do tego odwo³ywaæ w algorytmie
+	// tutaj zamieœciæ wierzcho³ki i krawêdzie
+	// graf = zbiór wierzcho³ków + zbiór krawêdzi...
+	// z wierzcho³ka trzeba wywaliæ pod³¹czone krawêdzie i wstawiæ tutaj, bo siê burdel robi
+	// po co ten sztuczny podzia³ na model, control i view? to co tu jest nie ma nic wspólnego z wzorcem MVC
+	// w krawêdzi potrzebny mi jest int, w którym bêdzie wartoœæ koloru, do algorytmu kolor swingowy mi jest delikatnie mówi¹c zbêdny
+	// RZECZ NAJWA¯NIEJSZA: mieszamy swinga z implementacj¹ w³aœciw¹ tego wszystkiego (czyli tak jak jest) czy coœ z tym robimy bardziej normalnego...? 
+	// nie chce tego sam poprawiaæ, bo jeszcze wiêkszy burdel z tego wyjdzie ni¿ jest
+}
