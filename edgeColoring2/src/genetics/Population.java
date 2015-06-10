0f5f0039c8b10d6664ad93f457d4eb379cfb636a
Xenir <xenirek@gmail.com>
2015-06-10 22:31:30 +0200

    Few functions to manage population

--- edgeColoring2/src/genetics/Population.java
+++ edgeColoring2/src/genetics/Population.java
@@ -9,8 +9,27 @@ public class Population {
 	private LinkedList<Chromosome> population;
 	
 	public Population() {
-		size = Vars.edges.size();
+		size = 0;
+		population = null;
+	}
+	
+	public Population(int size) {
+		this.size = size;
 		generateRandomPopulation();
+	}
+	
+	public Population(LinkedList<Chromosome> list) {
+		setPopulation(list);
+	}
+	
+	public Population(Population p) {
+		size = p.getSize();
+		setPopulation(p.getPopulationList());
+	}
+	
+	public void addChromosome(Chromosome ch) {
+		size++;
+		population.add(ch);
 	}
 	
 	public void generateRandomPopulation() {
@@ -21,9 +40,19 @@ public class Population {
 		}
 	}
 	
+	public void setPopulation(LinkedList<Chromosome> p) {
+		size = p.size();
+		population = new LinkedList<Chromosome>();
+		population.addAll(p);
+	}
+	
 	public void calculateFintess() {
 		for (Chromosome ch : population)
 			ch.calculateFitness();
+	}
+	
+	public Chromosome getChromosome(Integer index) {
+		return population.get(index);
 	}
 	
 	public String toString() {
@@ -33,6 +62,20 @@ public class Population {
 			result += i + ") " + ch + " " + ch.getFitness() + "\n"; 
 		}
 		return result;
+	}
+	
+	public void removeChromosome(Integer id) {
+		population.remove(id);
+	}
+	
+	public Object clone() {
+		Population copy = new Population();
+		copy.setPopulation(population);
+		return copy;
+	}
+	
+	public int getSize() {
+		return size;
 	}
 	
 	public LinkedList<Chromosome> getPopulationList() {
