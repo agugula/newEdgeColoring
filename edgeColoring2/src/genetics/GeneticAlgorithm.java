32d936e1e65896c3901a8c73d7b09fa8c44201d5
Xenir <xenirek@gmail.com>
2015-06-10 23:22:49 +0200

    Genetic algorithm implementation, not ready, it create new generation once and return the best result (not complete implementation, I will add a loop to create more generations)

--- edgeColoring2/src/genetics/GeneticAlgorithm.java
+++ edgeColoring2/src/genetics/GeneticAlgorithm.java
@@ -2,8 +2,27 @@ package genetics;
 
 import java.util.LinkedList;
 
+import model.Vars;
+
 public class GeneticAlgorithm {
-	public static LinkedList<Integer> run() {
-		return null;
+	
+	private Selector selector;
+	
+	public GeneticAlgorithm(Selector s) {
+		selector = s;
+	}
+	
+	public LinkedList<Integer> run() {
+		Population firstGeneration = new Population(Vars.population);
+		Population secondGeneration = new Population();
+		int counter = 0;
+		for ( ; counter < Vars.population * Vars.partToCrossover; counter += 2) {
+			Chromosome [] parents = selector.select(firstGeneration);
+			Chromosome newCh = RandomCrossover.crossover(parents);
+			Mutation.mutate(newCh);
+			secondGeneration.addChromosome(newCh);
+		}
+		firstGeneration = PopulationSelector.select(firstGeneration, secondGeneration, Vars.population);
+		return firstGeneration.getChromosome(0).getGenotype();
 	}
 }
