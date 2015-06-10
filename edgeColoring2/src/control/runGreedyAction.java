5a6817468854810b3ed2b7220a6e1e954b56f74a
Xenir <xenirek@gmail.com>
2015-06-10 12:45:44 +0200

    Added call of greedy algorithm

--- edgeColoring2/src/control/runGreedyAction.java
+++ edgeColoring2/src/control/runGreedyAction.java
@@ -1,9 +1,13 @@
 package control;
 
+import genetics.GreedyAlgorithm;
+
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+
+import model.Vars;
 
 public class runGreedyAction extends AbstractAction implements Action {
 
@@ -14,7 +18,7 @@ public class runGreedyAction extends AbstractAction implements Action {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-
+		Vars.parseColors(GreedyAlgorithm.run());
 	}
 
 }
