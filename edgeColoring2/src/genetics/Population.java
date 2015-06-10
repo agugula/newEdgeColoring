5d4b17308f9f8c74e3a86976ec2f7c457a66c0ae
Xenir <xenirek@gmail.com>
2015-06-10 15:42:14 +0200

    Population class

--- edgeColoring2/src/genetics/Population.java
+++ edgeColoring2/src/genetics/Population.java
@@ -2,15 +2,41 @@ package genetics;
 
 import java.util.LinkedList;
 
+import model.Vars;
+
 public class Population {
 	private int size = 0;
 	private LinkedList<Chromosome> population;
 	
-	public Population(int s) {
-		size = s;
+	public Population() {
+		size = Vars.edges.size();
+		generateRandomPopulation();
+	}
+	
+	public void generateRandomPopulation() {
+		population = new LinkedList<Chromosome>();
 		for (int i = 0; i < size; i++) {
 			population.add(new Chromosome());
-			population.get(i).generateRandomChromosome(s);
+			population.get(i).generateRandomChromosome(Vars.getMaximumNodeDegree() + 1);
 		}
 	}
+	
+	public void calculateFintess() {
+		for (Chromosome ch : population)
+			ch.calculateFitness();
+	}
+	
+	public String toString() {
+		String result = new String();
+		for (int i = 0; i < size; i++) {
+			Chromosome ch = population.get(i);
+			result += i + ") " + ch + " " + ch.getFitness() + "\n"; 
+		}
+		return result;
+	}
+	
+	public LinkedList<Chromosome> getPopulationList() {
+		return population;
+	}
+	
 }
